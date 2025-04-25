ae0e7b9d37b811353756ce28d52bb7e9bd3727cb
ywang3 <ywang3@YWANG3-CN-LE>
2025-04-25 10:27:41 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+using Tekla.Structures.Dialog;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Geometry3d;
+using Point = Tekla.Structures.Geometry3d.Point;
+using View = Tekla.Structures.Model.UI.View;
+
+namespace _2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var hand = new DrawingHandler();
+            var q = hand.GetDrawingObjectSelector();
+
+            var drawing = hand.GetActiveDrawing();
+
+            var all = q.GetSelected();
+            while (all.MoveNext())
+            {
+                var straight = all.Current  as WeldMark;
+                var b = straight.GetAxisAlignedBoundingBox();
+                //var s = straight.GetObjects();
+                //foreach (var item in s)
+                //{
+                //        //item 为其中每一个dimension的属性，可以获取对应的值
+                //}
+                //var tag = view.Attributes.TagsAttributes.TagA5;
+                //var tagt = tag.TagContent;
+                //var conlement = new ContainerElement();
+                //conlement.Frame.Type = FrameTypes.Circle;
+                //conlement.Frame.Color = DrawingColors.Blue;
+                //TextElement text = new TextElement("AAAAA");
+                //conlement.Add(text);
+                //tagt.Add(conlement);
+                //view.Modify();
+                //drawing.CommitChanges();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var model = new Model();
+            Picker picker = new Picker();
+            var bolt = picker.PickObjects(Picker.PickObjectsEnum.PICK_N_OBJECTS);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Model model = new Model();
+            AABB aabb = new AABB(new Point(-1000, -1000, -1000), new Point(1000, 1000,1000));
+            ModelViewEnumerator ViewEnum = ViewHandler.GetAllViews();
+            while (ViewEnum.MoveNext())
+            {
+                var view = ViewEnum.Current;
+                Tekla.Structures.Model.UI.ViewHandler.RedrawView(view);
+                view.WorkArea = aabb;
+                view.Modify();
+                model.CommitChanges();
+           
+            }
+
+        }
+    }
+}
